99d804b5f4935640b6f20a831ee93cb889c75678
zxy <zxy18310546374@163.com>
2019-09-18 10:46:48 +0800

    domain

---
+++ take_domain/src/main/java/com/mr/take_domain/TakeDomainApplication.java
@@ -0,0 +1,13 @@
+package com.mr.take_domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeDomainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TakeDomainApplication.class, args);
+    }
+
+}
